3e823643ab78b3c99caac3d1c8f547dbe909a8ca
yuramorozov01 <yura_morozov01@mail.ru>
2021-05-04 00:25:31 +0300

    feat: add serializer for movie details (ML-7)

--- movielib/movies/views.py
+++ movielib/movies/views.py
@@ -12,3 +12,11 @@ class MovieListView(APIView):
 		movies = Movie.objects.filter(draft=False)
 		serializer = MovieListSerializer(movies, many=True)
 		return Response(serializer.data)
+
+
+class MovieDetailsSerializer(serializers.ModelSerializer):
+	'''Movie details'''
+
+	class Meta:
+		model = Movie
+		exclude = ('draft',)
