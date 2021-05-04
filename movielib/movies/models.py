71c17f6e9ec183afa7db507300042b4c83269b77
yuramorozov01 <yura_morozov01@mail.ru>
2021-05-04 16:28:18 +0300

    feat: add related children name to review (ML-8)

--- movielib/movies/models.py
+++ movielib/movies/models.py
@@ -130,7 +130,7 @@ class Review(models.Model):
     email = models.EmailField()
     name = models.CharField('Name', max_length=100)
     text = models.TextField('Text', max_length=5000)
-    parent = models.ForeignKey('self', verbose_name='Parent', on_delete=models.SET_NULL, blank=True, null=True)
+    parent = models.ForeignKey('self', verbose_name='Parent', on_delete=models.SET_NULL, blank=True, null=True, related_name='children')
     movie = models.ForeignKey(Movie, verbose_name='Movie', on_delete=models.CASCADE, related_name='reviews')
 
     def __str__(self):
