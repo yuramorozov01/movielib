b50486d3b3445e6647346d63d4ef9482462de8e7
yuramorozov01 <yura_morozov01@mail.ru>
2021-05-04 16:05:00 +0300

    feat: add view for creating reviews (ML-8)

--- movielib/movies/views.py
+++ movielib/movies/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Movie
-from .serializers import MovieListSerializer, MovieDetailsSerializer
+from .serializers import MovieListSerializer, MovieDetailsSerializer, ReviewCreateSerializer
 
 
 class MovieListView(APIView):
@@ -21,3 +21,14 @@ class MovieDetailsView(APIView):
 		movie = Movie.objects.get(id=movie_id, draft=False)
 		serializer = MovieDetailsSerializer(movie)
 		return Response(serializer.data)
+
+
+class ReviewCreateView(APIView):
+	'''Create movie review'''
+
+	def post(self, request):
+		review = ReviewCreateSerializer(data=request.data)
+		if review.is_valid():
+			review.save()
+			
+		return Response(status=201)
