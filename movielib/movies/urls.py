dd7eb19ec3010ecaf03547b8e419efefc9d26988
yuramorozov01 <yura_morozov01@mail.ru>
2021-05-04 16:10:01 +0300

    feat: add url path for creating review (ML-8)

--- movielib/movies/urls.py
+++ movielib/movies/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
 	path('movie/', views.MovieListView.as_view()),
 	path('movie/<int:movie_id>/', views.MovieDetailsView.as_view()),
+	path('review/', views.ReviewCreateView.as_view()),
 ]
