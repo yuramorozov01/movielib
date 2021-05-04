1f9725dbcd1b115d77f13070b203b40a05011b94
yuramorozov01 <yura_morozov01@mail.ru>
2021-05-04 19:31:01 +0300

    feat: add url path for creating/updating rating (ML-9)

--- movielib/movies/urls.py
+++ movielib/movies/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	path('movie/', views.MovieListView.as_view()),
 	path('movie/<int:movie_id>/', views.MovieDetailsView.as_view()),
 	path('review/', views.ReviewCreateView.as_view()),
+	path('rating/', views.AddStarRatingView.as_view()),
 ]
