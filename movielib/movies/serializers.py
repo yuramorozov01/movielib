7d8561094b655072df369e8540b9bd3de0a7d757
yuramorozov01 <yura_morozov01@mail.ru>
2021-05-04 16:28:45 +0300

    feat: add recursive review output with children reviews (ML-8)

--- movielib/movies/serializers.py
+++ movielib/movies/serializers.py
@@ -11,6 +11,22 @@ class MovieListSerializer(serializers.ModelSerializer):
 		fields = ('title', 'tagline', 'category')
 
 
+class RecursiveReviewChildrenSerializer(serializers.Serializer):
+	'''Recursive review children output'''
+
+	def to_representation(self, value):
+		serializer = self.parent.parent.__class__(value, context=self.context)
+		return serializer.data
+
+
+class FilterReviewListSerializer(serializers.ListSerializer):
+	'''Filter for reviews to output only parent reviews'''
+
+	def to_representation(self, data):
+		data = data.filter(parent=None)
+		return super().to_representation(data)
+		
+
 class ReviewCreateSerializer(serializers.ModelSerializer):
 	'''Create review'''
 
@@ -22,9 +38,12 @@ class ReviewCreateSerializer(serializers.ModelSerializer):
 class ReviewSerializer(serializers.ModelSerializer):
 	'''Output review'''
 
+	children = RecursiveReviewChildrenSerializer(many=True)
+
 	class Meta:
+		list_serializer_class = FilterReviewListSerializer
 		model = Review
-		fields = ('name', 'text', 'parent')
+		fields = ('name', 'text', 'children')
 
 
 class MovieDetailsSerializer(serializers.ModelSerializer):
