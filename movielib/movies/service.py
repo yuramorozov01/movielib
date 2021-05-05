a08b6cae717bbfc5213ac0365ad4f0bcdc88b10a
yuramorozov01 <yura_morozov01@mail.ru>
2021-05-06 00:39:56 +0300

    feat: add pagination (ML-18)

--- movielib/movies/service.py
+++ movielib/movies/service.py
@@ -1,5 +1,24 @@
 from django_filters import rest_framework as filters
+
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
 from movies.models import Movie
+
+
+class PaginationMovies(PageNumberPagination):
+	page_size = 2
+	max_page_size = 100
+
+	def get_paginated_response(self, data):
+		return Response({
+			'links': {
+				'next': self.get_next_link(),
+				'previous': self.get_previous_link(),
+			},
+			'count': self.page.paginator.count,
+			'results': data,
+		})
 
 
 def get_client_ip(request):
