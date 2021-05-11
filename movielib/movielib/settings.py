be695c86888aa3a5d11b1e7e5d181dd4c1e980d8
yuramorozov01 <yura_morozov01@mail.ru>
2021-05-11 23:00:23 +0300

    feat: temporarily increased token lifetime

--- movielib/movielib/settings.py
+++ movielib/movielib/settings.py
@@ -271,8 +271,8 @@ DJOSER = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 
